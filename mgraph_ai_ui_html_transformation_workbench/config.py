e7cca064089cc4fa3e1528fe19b226dbc17fc8d9
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-29 15:21:48 +0000

    added fast_api support added /dev-briefs folder refactored workbench code into correct v0/v0.1 folder

---
+++ mgraph_ai_ui_html_transformation_workbench/config.py
@@ -0,0 +1,12 @@
+from mgraph_ai_ui_html_transformation_workbench import package_name
+
+SERVICE_NAME                                 = package_name
+FAST_API__TITLE                              = "Html Transformation Workbench"
+FAST_API__DESCRIPTION                        = "MGraph AI UI - Html Transformation Workbench"
+LAMBDA_DEPENDENCIES__HTML_TRANSFORMATION__UI = ['osbot-fast-api-serverless==v1.33.0'     ]
+
+
+UI__CONSOLE__ROUTE__CONSOLE    = 'workbench'
+UI__CONSOLE__ROUTE__START_PAGE = 'index'
+UI__CONSOLE__MAJOR__VERSION    = "v0/v0.1"
+UI__CONSOLE__LATEST__VERSION   = "v0.1.0"
