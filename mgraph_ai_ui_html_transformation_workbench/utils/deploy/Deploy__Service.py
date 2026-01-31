e25b1c9a298856c2201d9d0ff94d466e574d372b
Dinis Cruz <dinis.cruz@owasp.org>
2026-01-31 01:37:25 +0000

    added ui folder for deployment

--- mgraph_ai_ui_html_transformation_workbench/utils/deploy/Deploy__Service.py
+++ mgraph_ai_ui_html_transformation_workbench/utils/deploy/Deploy__Service.py
@@ -1,3 +1,4 @@
+import mgraph_ai_ui_html_transformation_workbench__ui
 from osbot_fast_api_serverless.deploy.Deploy__Serverless__Fast_API      import Deploy__Serverless__Fast_API
 from mgraph_ai_ui_html_transformation_workbench.config                  import LAMBDA_DEPENDENCIES__HTML_TRANSFORMATION__UI, SERVICE_NAME
 from mgraph_ai_ui_html_transformation_workbench.fast_api.lambda_handler import run
@@ -8,6 +9,7 @@ class Deploy__Service(Deploy__Serverless__Fast_API):
 
     def deploy_lambda(self):
         with super().deploy_lambda() as _:
+            _.add_folder(mgraph_ai_ui_html_transformation_workbench__ui.path)
             return _
 
     def handler(self):
