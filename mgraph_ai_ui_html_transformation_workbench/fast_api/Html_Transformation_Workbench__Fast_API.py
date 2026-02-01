78ec7993a0dbf262c614d2a7f65b7f54bb7366aa
Dinis Cruz <dinis.cruz@owasp.org>
2026-02-01 16:35:55 +0000

    added support for running locally with access to the actually issues stored in this repo refactored the existing issues into the new architecture

--- mgraph_ai_ui_html_transformation_workbench/fast_api/Html_Transformation_Workbench__Fast_API.py
+++ mgraph_ai_ui_html_transformation_workbench/fast_api/Html_Transformation_Workbench__Fast_API.py
@@ -1,5 +1,6 @@
 import mgraph_ai_ui_html_transformation_workbench__ui
 from fastapi                                                                                    import Response
+from osbot_utils.utils.Env                                                                      import get_env
 from osbot_utils.utils.Files                                                                    import path_combine, file_contents
 from memory_fs.Memory_FS                                                                        import Memory_FS
 from osbot_fast_api_serverless.fast_api.routes.Routes__Info                                     import Routes__Info
@@ -30,12 +31,14 @@ from mgraph_ai_ui_html_transformation_workbench.utils.Version                   
 ROUTES_PATHS__CONSOLE = [f'/{UI__CONSOLE__ROUTE__CONSOLE}',
                          '/events/server']
 
-DEFAULT__ISSUES_PATH  = '.issues'
+DEFAULT__ISSUES_PATH       = '.issues'
+ENV_VAR__ISSUES__IN_MEMORY = 'ISSUES__IN_MEMORY'                                 # Set to 'false' to use local disk
+ENV_VAR__ISSUES__PATH      = 'ISSUES__PATH'                                      # Path to .issues folder
 
 class Html_Transformation_Workbench__Fast_API(Serverless__Fast_API):
-    run_in_memory   : bool                 = True                                 # todo: find a better place to put this option
-    issues_path     : Safe_Str__File__Path = DEFAULT__ISSUES_PATH
-    memory_fs       : Memory_FS            = None                                 # todo: refactor into separate project
+    run_in_memory   : bool                 = True                                # Default to memory, override via env var
+    issues_path     : Safe_Str__File__Path = DEFAULT__ISSUES_PATH                # Default path, override via env var
+    memory_fs       : Memory_FS            = None
 
     graph_repository      : Graph__Repository    = None
     link_service          : Link__Service        = None
@@ -54,8 +57,6 @@ class Html_Transformation_Workbench__Fast_API(Serverless__Fast_API):
             _.version        = version__mgraph_ai_service__html_transformation_workbench
             _.description    = FAST_API__DESCRIPTION
 
-            #_.enable_api_key = False        # because of chrome-llm/manifest.json
-            #self.add_chrome_llm_routes()    # todo: refactor this into separate project
             self.setup_services()
 
         return super().setup()
@@ -71,19 +72,21 @@ class Html_Transformation_Workbench__Fast_API(Serverless__Fast_API):
     # ═══════════════════════════════════════════════════════════════════════════════
     # Create all services with proper dependency chain
     # ═══════════════════════════════════════════════════════════════════════════════
-    def setup_services(self):                                                               # Initialize the service dependency chain.
+    def setup_services(self):                                                               # Initialize the service dependency chain
 
-        if self.run_in_memory:                                                              # 1. Create storage backend based on configuration
+        use_memory  = self.resolve_storage_mode()                                           # 1. Determine storage mode from env var or attribute
+        issues_path = self.resolve_issues_path()                                            # 2. Determine issues path from env var or attribute
+
+        if use_memory:                                                                      # 3. Create storage backend based on configuration
             storage_fs = Storage_FS__Memory()
         else:
-            storage_fs = Storage_FS__Local_Disk(root_path=self.issues_path)
+            storage_fs = Storage_FS__Local_Disk(root_path=issues_path)
 
+        self.memory_fs = Memory_FS(storage_fs=storage_fs)                                   # 4. Create Memory-FS wrapper
 
-        self.memory_fs = Memory_FS(storage_fs=storage_fs)                                   # 2. Create Memory-FS wrapper
+        self.graph_repository = Graph__Repository(memory_fs  = self.memory_fs       )       # 5. Create repository
 
-        self.graph_repository = Graph__Repository(memory_fs  = self.memory_fs       )       # 3. Create repository
-
-        self.type_service  = Type__Service (repository=self.graph_repository)               # 4. Create services
+        self.type_service  = Type__Service (repository=self.graph_repository)               # 6. Create services
         self.node_service  = Node__Service (repository=self.graph_repository)
         self.link_service  = Link__Service (repository=self.graph_repository)
 
@@ -97,8 +100,23 @@ class Html_Transformation_Workbench__Fast_API(Serverless__Fast_API):
                                                                types_service   = self.types_status__service  ,
                                                                index_service   = self.index_status__service  )
 
-        # todo: see how this works, since this should be loaded from the repo
-        self.type_service.initialize_default_types()                                        # 5. Initialize default types
+        self.type_service.initialize_default_types()                                        # 7. Initialize default types (skips if already exist)
+
+    def resolve_storage_mode(self) -> bool:                                                 # Determine if using in-memory storage
+        env_value = get_env(ENV_VAR__ISSUES__IN_MEMORY, None)                               # Check environment variable first
+
+        if env_value is not None:
+            return env_value.lower() not in ('false', '0', 'no', 'off')                     # Any of these = use disk
+
+        return self.run_in_memory                                                           # Fall back to instance attribute
+
+    def resolve_issues_path(self) -> str:                                                   # Determine issues folder path
+        env_value = get_env(ENV_VAR__ISSUES__PATH, None)                                    # Check environment variable first
+
+        if env_value:
+            return env_value
+
+        return str(self.issues_path)                                                        # Fall back to instance attribute
 
     def add_chrome_llm_routes(self):
         def get_file(file_path):
@@ -120,8 +138,6 @@ class Html_Transformation_Workbench__Fast_API(Serverless__Fast_API):
         self.add_route_get(manifest_json)
 
 
-
-    # todo: refactor to separate class (focused on setting up this static route)
     def setup_static_routes(self):
         path_static_folder  = mgraph_ai_ui_html_transformation_workbench__ui.path
         path_static         = f"/{UI__CONSOLE__ROUTE__CONSOLE}"
@@ -138,11 +154,3 @@ class Html_Transformation_Workbench__Fast_API(Serverless__Fast_API):
             return RedirectResponse(url=path_latest_version)
 
         self.add_route_get(redirect_to_latest)
-
-        # todo: find way to make this deploy to the lambda function
-        #self.add_static_route__dev_briefs()
-
-    # def add_static_route__dev_briefs(self):
-    #     dev_briefs__folder = path_combine(mgraph_ai_ui_html_transformation_workbench.path, '../docs/dev-briefs' )
-    #     dev_briefs__path   = '/dev-briefs'
-    #     self.app().mount(dev_briefs__path, StaticFiles(directory=dev_briefs__folder), name='dev-briefs')
